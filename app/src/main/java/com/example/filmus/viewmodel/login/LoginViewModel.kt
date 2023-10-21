53454241142d818c2bcc929b65513a15a7d05a27
Alexander <73055263+cah999@users.noreply.github.com>
2023-10-21 19:24:06 +0700

    fix: LoginViewModel problem fix

--- app/src/main/java/com/example/filmus/viewmodel/login/LoginViewModel.kt
+++ app/src/main/java/com/example/filmus/viewmodel/login/LoginViewModel.kt
@@ -2,13 +2,10 @@ package com.example.filmus.viewmodel.login
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.filmus.api.ApiService
-import com.example.filmus.api.LoginResponse
 import com.example.filmus.api.createApiService
 import com.example.filmus.domain.model.LoginResult
 import com.example.filmus.domain.model.LoginUseCase
 import com.example.filmus.repository.ApiLoginRepository
-import com.example.filmus.repository.LoginRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
@@ -21,6 +18,7 @@ class LoginViewModel() : ViewModel() {
             val loginUseCase = LoginUseCase(loginRepository)
             val result = loginUseCase.login(username, password)
             onResult(result)
+
         }
     }
 }
