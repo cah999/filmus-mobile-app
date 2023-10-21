6e3a3d44828a31a2bfde3b453eef89e192b92409
Alexander <73055263+cah999@users.noreply.github.com>
2023-10-21 20:08:57 +0700

    feature: Login screen done using figma layout

--- app/src/main/java/com/example/filmus/ui/screens/WelcomeScreen.kt
+++ app/src/main/java/com/example/filmus/ui/screens/WelcomeScreen.kt
@@ -1,18 +1,32 @@
 package com.example.filmus.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.filmus.R
 import com.example.filmus.navigation.Screen
 
 @Composable
@@ -22,21 +36,93 @@ fun WelcomeScreen(navController: NavHostController) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Welcome to Filmus")
+        Image(
+            painter = painterResource(id = R.drawable.welcome_logo),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(35.dp)
+        )
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "Погрузись в мир кино", style = TextStyle(
+                fontSize = 20.sp,
+                lineHeight = 24.sp,
+                fontFamily = FontFamily(Font(R.font.inter)),
+                fontWeight = FontWeight(700),
+                color = Color(0xFFFFFFFF),
+                textAlign = TextAlign.Center,
+            )
+        )
 
-        Button(
-            onClick = { navController.navigate(Screen.Login.route) },
-            modifier = Modifier.padding(8.dp)
-        ) {
-            Text(text = "Login")
-        }
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            text = "Мы предлагаем удобный и легкий способ насладиться любимыми фильмами прямо с Вашего мобильного устройства.",
+            style = TextStyle(
+                fontSize = 15.sp,
+                fontFamily = FontFamily(Font(R.font.inter)),
+                fontWeight = FontWeight(400),
+                color = Color(0xFFFFFFFF),
+
+                textAlign = TextAlign.Center,
+            )
+        )
+
+        Spacer(modifier = Modifier.height(35.dp))
+
         Button(
             onClick = { navController.navigate(Screen.Registration.route) },
-            modifier = Modifier.padding(8.dp)
+            modifier = Modifier
+                .width(328.dp)
+                .height(42.dp),
+
+            shape = RoundedCornerShape(size = 10.dp),
+            colors = androidx.compose.material3.ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFFC315E),
+            ),
+            contentPadding = PaddingValues(start = 15.dp, top = 12.dp, end = 15.dp, bottom = 12.dp)
+
+
         ) {
-            Text(text = "Register")
+            Text(
+                text = "Регистрация",
+
+                style = TextStyle(
+                    fontSize = 15.sp,
+                    fontFamily = FontFamily(Font(R.font.inter)),
+                    fontWeight = FontWeight(600),
+                    color = Color(0xFFFFFFFF),
+                    textAlign = TextAlign.Center,
+                )
+            )
+        }
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        Button(
+            onClick = { navController.navigate(Screen.Registration.route) },
+            modifier = Modifier
+                .width(328.dp)
+                .height(42.dp),
+
+            shape = RoundedCornerShape(size = 10.dp),
+            colors = androidx.compose.material3.ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF292929)
+            ),
+            contentPadding = PaddingValues(start = 15.dp, top = 12.dp, end = 15.dp, bottom = 12.dp)
+        ) {
+            Text(
+                text = "Войти",
+
+                style = TextStyle(
+                    fontSize = 15.sp,
+                    fontFamily = FontFamily(Font(R.font.inter)),
+                    fontWeight = FontWeight(600),
+                    color = Color(0xFFFC315E),
+                    textAlign = TextAlign.Center,
+                )
+            )
         }
     }
 }
