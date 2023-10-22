37d505b28bfdb3c3f709e4ad0f599cef55ab3717
Alexander <73055263+cah999@users.noreply.github.com>
2023-10-22 18:14:58 +0700

    fix: buttons padding fix

--- app/src/main/java/com/example/filmus/ui/screens/WelcomeScreen.kt
+++ app/src/main/java/com/example/filmus/ui/screens/WelcomeScreen.kt
@@ -3,7 +3,6 @@ package com.example.filmus.ui.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
