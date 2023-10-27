a574908685dbae9652009ff3e719a183c3739573
Alexander <73055263+cah999@users.noreply.github.com>
2023-10-27 23:54:35 +0700

    feature: added bottom bar to main screen

--- app/src/main/java/com/example/filmus/ui/screens/main/MainScreen.kt
+++ app/src/main/java/com/example/filmus/ui/screens/main/MainScreen.kt
@@ -2,19 +2,29 @@ package com.example.filmus.ui.screens.main
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import com.example.filmus.navigation.AppNavigator
+import com.example.filmus.navigation.BottomBar
 
 @Composable
 fun MainScreen(navController: NavController, appNavigator: AppNavigator) {
-    Box(
-        contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()
-    ) {
-        CircularProgressIndicator()
-    }
+    Scaffold(bottomBar = {
+        BottomBar(navController = navController, appNavigator = appNavigator)
+    }, content = {
+        Box(
+            contentAlignment = Alignment.Center, modifier = Modifier
+                .padding(it)
+                .fillMaxSize()
+        ) {
+            CircularProgressIndicator()
+        }
+    })
+
 }
 
